41bf4ce1a5c8ca71588cc7ff71038202338914bc
pikesley <sam.pikesley@gmail.com>
2020-01-19 13:43:55 +0000

    add some rudimentary configuration

---
+++ shobaleader-one/tests/unit_test_conf.py
@@ -0,0 +1,9 @@
+from lib.conf import config
+
+
+def test_config():
+    """Test it reads the config correctly."""
+    path = 'tests/fixtures/config/config.yaml'
+    assert config(path)['something']['bar'] == [
+        'apple', 'banana', 'carrot'
+    ]
