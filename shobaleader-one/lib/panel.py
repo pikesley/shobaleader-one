29a2c54772c3302594a5e013b67b3d809420893d
pikesley <sam.pikesley@gmail.com>
2020-01-19 15:16:00 +0000

    generate the correct Pixels for the platform

--- shobaleader-one/lib/panel.py
+++ shobaleader-one/lib/panel.py
@@ -1,4 +1,12 @@
+from platform import platform
+
 import lib.conf
+
+ARM = False
+if 'arm' in platform():  # nocov
+    ARM = True
+    from neopixel import NeoPixel
+    import board
 
 
 class Panel:
@@ -10,4 +18,28 @@ class Panel:
         self.width = self.config['width']
         self.height = self.config['height']
         self.length = self.width * self.height
+        self.pixels = self.appropriate_pixels()
 
+    def appropriate_pixels(self):
+        """We can only have Real Pixels on an Actual Pi."""
+        if ARM:
+            return NeoPixel(
+                board.D18,
+                self.length,
+                auto_write=False)  # nocov
+        else:
+            return FakePixel(self.length)
+
+        # self.current_performer = None
+        # self.current_args = None
+        # self.process = None
+
+
+class FakePixel(list):
+    """Fake NeoPixels for testing."""
+
+    def __init__(self, length):  # pylint: disable=W0231
+        """Construct."""
+        self.length = length
+        for _ in range(self.length):
+            self.append((0, 0, 0))
--- shobaleader-one/tests/unit_test_panel.py
+++ shobaleader-one/tests/unit_test_panel.py
@@ -1,17 +1,23 @@
 from mock import patch
 
-from lib.panel import Panel
+from lib.panel import FakePixel, Panel
 
 
-class TestPanel:
+@patch('lib.conf.config',
+       return_value={'panel': {'width': 16, 'height': 4}})
+class TestPanel:  # pylint: disable=W0613
     """Test the Panel."""
 
-    @patch('lib.conf.config',
-           return_value={'panel': {'width': 16, 'height': 4}})
-    def test_constructor(self, config):  # pylint: disable=W0613
+    def test_constructor(self, config):
         """Test it configures itself correctly."""
         panel = Panel()
 
         assert panel.width == 16
         assert panel.height == 4
         assert panel.length == 64
+
+    def test_correct_pixels(self, config):
+        """Test it gets FakePixels in test."""
+        panel = Panel()
+
+        assert isinstance(panel.pixels, FakePixel)
