9bd84c672aad63ca72fcfc10602f4b3d03c5e2d7
pikesley <sam.pikesley@gmail.com>
2020-01-31 20:08:54 +0000

    Never discount the possibility of me being a fuckwit

--- shobaleader-one/tests/test_yeeter.py
+++ shobaleader-one/tests/test_yeeter.py
@@ -1,5 +1,6 @@
 from lib.yeeter import yeet
 
+
 def test_yeeting():
     """Test the Yeeter."""
     cases = (
@@ -7,7 +8,7 @@ def test_yeeting():
         (1, 2, [(0, 1), (0, 0)]),
         (2, 1, [(0, 0), (1, 0)]),
         (2, 2, [(0, 1), (0, 0), (1, 0)]),
-        (3, 3, [(0, 2), (0, 1), (0, 0), (1, 0), (2, 0)])
+        (3, 3, [(0, 2), (0, 1), (0, 0), (1, 0), (2, 0)]),
     )
 
     for yeetee_width, grid_width, expected in cases:
