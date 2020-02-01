7240b26361115429bb6ea801165ffd7f544a730f
pikesley <sam.pikesley@gmail.com>
2020-02-01 15:09:46 +0000

    use the yeeter's generator

--- shobaleader-one/lib/performers/marquee.py
+++ shobaleader-one/lib/performers/marquee.py
@@ -4,7 +4,7 @@ from lib.conf import config
 from lib.light_grid import LightGrid
 from lib.performers.performer import Performer
 from lib.text_banner import TextBanner
-from lib.yeeter import yeet
+from lib.yeeter import yeeter
 
 
 class Marquee(Performer):
@@ -16,14 +16,17 @@ class Marquee(Performer):
         self.defaults = {"colour": [255, 0, 0], "delay": 0.01}
         self.apply_defaults()
         self.config = config()
+        self.banner = TextBanner(self.text)
 
     def perform(self):
         """Keep sending frames."""
         while True:  # nocov
-            indeces = yeet(self.banner.width, self.config["panel"]["width"])
-            for index, offset in indeces:
+            for index, offset in yeeter(
+                self.banner.width, self.config["panel"]["width"]
+            ):
                 pixels = self.banner.carve_slice(index)
                 grid = LightGrid(pixels, offset)
+                grid.map({1: self.colour})
 
                 yield grid
                 sleep(self.delay)
